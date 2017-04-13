244984b6e9bc2b080569dc0206166065b482abb8
Julita Ołtusek <julita.oltusek@gmail.com>
2017-04-13 17:46:20 +0200

    Test added

--- src/main/java/com/joltusek/GreetingController.java
+++ src/main/java/com/joltusek/GreetingController.java
@@ -1,10 +1,12 @@
 package com.joltusek;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 
 /**
  * Created by Julita Ołtusek on 13.04.2017.
@@ -15,7 +17,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value="/greeting", method = RequestMethod.GET)
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
--- src/test/java/com/joltusek/PikSbApplicationTests.java
+++ src/test/java/com/joltusek/PikSbApplicationTests.java
@@ -2,15 +2,24 @@ package com.joltusek;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PikSbApplicationTests {
 
+	@Autowired
+	private GreetingController controller;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
+
+	
 
 }
