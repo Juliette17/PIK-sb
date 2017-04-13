e1a2c228d8ca948a85d9e629f7d0188db6fbd7ae
Julita Ołtusek <julita.oltusek@gmail.com>
2017-04-13 16:55:00 +0200

    Initial commit

---
+++ src/test/java/com/joltusek/PikSbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joltusek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PikSbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
